2b884233c4b277427e3a0cd0880ac3c7fecacee3
waka <d950@protonmail.com>
2026-06-03 08:44:28 +0300

    Moved to Kitty for now, fixed a bug in fontsconfig

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@
 fish_vi_key_bindings
 
 # environment variables
-export TERMINAL=wezterm
+export TERMINAL=kitty
 export MPD_HOST="master@$HOME/.mpd/socket"
 export EDITOR=vim
 export MANPAGER='nvim +Man!'
